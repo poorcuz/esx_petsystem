7cbc8cdc335bdbc50031b7d13cb440506714b377
memzlol69 <github.stoplight379@passinbox.com>
2025-05-12 17:22:31 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,12 @@ fx_version 'adamant'
 
 game 'gta5'
 lua54 'yes'
+author 'poorcuz'
 
 shared_script '@es_extended/imports.lua'
 shared_script 'config/**.lua'
 
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
 	'server/**/**.lua'
 }
 
